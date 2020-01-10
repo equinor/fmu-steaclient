115ac172eb1a2d3e8c77e0a40c2f82cc4fc963e8
Øyvind Eide <oyveid@equinor.com>
2020-01-10 12:01:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Extension
+from setuptools import setup
 
 long_description = """
 =======
@@ -15,13 +15,24 @@ Introduction
 setup(name='stea',
       description='stea - calculate economic analysis',
       long_description=long_description,
-      author='Statoil ASA',
-      author_email='joaho@statoil.com',
-      url='https://github.com/Statoil/stea',
+      author="Software Innovation Bergen, Equinor ASA",
+      url='https://github.com/equinor/stea',
       packages=['stea'],
       setup_requires=['setuptools_scm'],
-      use_scm_version={'write_to' : 'stea/version.py'},
+      use_scm_version={'write_to': 'stea/version.py'},
       platforms='any',
       install_requires=['requests','pyyaml','configsuite'],
       test_suite='tests.suite',
-)
+      classifiers=[
+            "Development Status :: 1 - Planning",
+            "Environment :: Plugins",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+            "Natural Language :: English",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      )
