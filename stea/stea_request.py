6f0dc219bfa9006f2e29b509844365dccdc6f0f0
Håvard Berland <havb@equinor.com>
2022-05-02 10:20:21 +0200

    Pylint code, and add pylint to ci

--- stea/stea_request.py
+++ stea/stea_request.py
@@ -1,16 +1,17 @@
 import datetime
 import sys
+
 from .stea_keys import SteaKeys
 
 
-def date_string(dt):
-    return dt.strftime("%Y-%m-%dT%H:%M:%S")
+def date_string(timestamp):
+    return timestamp.strftime("%Y-%m-%dT%H:%M:%S")
 
 
 BARRELS_PR_SM3 = 6.2898
 
 
-class SteaRequest(object):
+class SteaRequest:
     units = {"Bbl": {"SM3": BARRELS_PR_SM3}, "Sm3": {"SM3": 1.0}}
 
     scale_factors = {"1": 1.0, "Mill": 1.0e-6, "1000 Mill": 1.0e-9}
@@ -28,7 +29,7 @@ class SteaRequest(object):
 
     def add_profile(self, profile_id, start_year, data):
         if not self.project.has_profile(profile_id):
-            raise KeyError("Invalid profile id:{} for this project".format(profile_id))
+            raise KeyError(f"Invalid profile id: {profile_id} for this project")
 
         profile_data = {
             SteaKeys.PROFILE_ID: profile_id,
@@ -49,9 +50,14 @@ class SteaRequest(object):
         key,
         first_year=None,
         last_year=None,
-        multiplier=[1],
+        multiplier=None,
         global_multiplier=1,
     ):
+        # pylint: disable=too-many-arguments
+        # pylint: disable=too-many-locals
+        if multiplier is None:
+            multiplier = [1]
+
         if self.stea_input.ecl_case is None:
             raise ValueError(
                 "When adding ecl_profile you must configure an Eclipse case"
@@ -59,7 +65,7 @@ class SteaRequest(object):
 
         case = self.stea_input.ecl_case
         if key not in case:
-            raise KeyError("No such summary key: {}".format(key))
+            raise KeyError(f"No such summary key: {key}")
 
         if first_year is None:
             start_date = case.start_date
@@ -81,11 +87,15 @@ class SteaRequest(object):
         else:
             unitfactor = 1.0
             sys.stdout.write(
-                "Default conversion between %s and %s to 1.\n" % (unit, ecl_unit)
+                f"Default conversion between {unit} and {ecl_unit} to 1.\n"
             )
         unit_conversion = unitfactor * self.scale_factors[mult]
         data = list(case.blocked_production(key, time_range) * unit_conversion)
-        ni = min(len(multiplier), len(data))
-        data[:ni] = map(lambda x, y: x * y, data[:ni], multiplier[:ni])
+
+        mult_rangeend = min(len(multiplier), len(data))
+        data[:mult_rangeend] = map(
+            lambda x, y: x * y, data[:mult_rangeend], multiplier[:mult_rangeend]
+        )
+
         data = [x * global_multiplier for x in data]
         self.add_profile(profile_id, first_year, data)
